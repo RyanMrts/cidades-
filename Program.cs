b48d1413ce9c5de6294733c1a9a78c1b753fdca1
Ryan <ryanmartinssilva2006@gmail.com>
2023-03-09 09:52:26 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Cidades do Litoral Sul de São Paulo");
+Console.WriteLine();
+Console.WriteLine("Cananéia");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Iguape");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Santos");
+Console.WriteLine("GUarujá");
+Console.WriteLine("Bertioga");
+Console.WriteLine("São Sebastião");
+Console.WriteLine("Ihabela");
+Console.WriteLine("Caraguatatuba");
+Console.WriteLine("Ubatuba");
